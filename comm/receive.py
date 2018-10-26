87f8118fa6bdd401c32eda9d72ad4aaf3e0f8919
UAVs at Berkeley <uavs.berkeley@berkeley.edu>
2018-10-26 01:18:05 -0700

    Updated receive and send

--- comm/receive.py
+++ comm/receive.py
@@ -1,16 +1,21 @@
 import socket
+from consts import *
 
 def receive(ip="0.0.0.0", port=5005):
-    UDP_IP = ip
-    UDP_PORT = port
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((UDP_IP, UDP_PORT))
+    UDP_IP = ip_receive
+    UDP_PORT = port_receive
 
     # TODO: Remove this later
     counter = 5
     while counter >= 0:
-        data, addr = sock.recvfrom(1024)
-        print "received message:", data
-        counter -= 1
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
+        sock.settimeout(1)
+        try:
+            data, addr = sock.recvfrom(1024)
+            print "received message:", data
+        except:
+            print "no message received"
+        finally:
+            counter -= 1
 
--- comm/send.py
+++ comm/send.py
@@ -1,15 +1,16 @@
 import socket
+from consts import *
 
 def send(ip="192.168.1.6", port=5005):
-    UDP_IP = ip
-    UDP_PORT = port
-    MESSAGE = "HELLO WORLD"
+    UDP_IP = ip_send
+    UDP_PORT = port_send
+    MESSAGE = message
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
     # TODO: Remove this later:
     counter = 5
-    while counter >= 5:
+    while counter >= 0:
         print(counter)
         sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
         counter -= 1
